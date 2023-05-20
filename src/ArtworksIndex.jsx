fb128fc8f42a88506f07a4e7fcccadab04d82be9
Sam Wells <sfwells0518@gmail.com>
2023-05-20 10:53:57 -0500

    adjust nav bar styling color and button colors

--- src/ArtworksIndex.jsx
+++ src/ArtworksIndex.jsx
@@ -1,19 +1,26 @@
 export function ArtworksIndex(props) {
   return (
-    <div>
-      <h1>All Artwork</h1>
-      {props.artworks.map((artwork) => (
-        <div key={artwork.id}>
-          <h3>{artwork.name}</h3>
-          <p>${artwork.price}</p>
-          <img src={artwork.image} />
-          <p>{artwork.description}</p>
-
-        </div>
-
-      
-      ))}
-
+    <div id="artworks-index" className="mt-4">
+      <center>
+        <h1>All Artwork</h1>
+      </center>
+      <div className="row">
+        {props.artworks.map((artwork) => (
+          <div key={artwork.id} className="col-sm-4 mb-3">
+            <div className="card h-100">
+              <div className="card-body card-body-custom d-flex flex-column">
+                <h4 className="card-title">{artwork.name}</h4>
+                <h6 className="card-price">${artwork.price}</h6>
+                <img src={artwork.image} className="card-img" alt={artwork.name} />
+                <p className="card-text">{artwork.description}</p>
+                <div className="mt-auto text-center">
+                  <button className="green-button">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,114 @@
 export function Header() {
   return (
-    <header>
-      <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
-      </nav>
-    </header>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Gallerize ||
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="nav nav-underline">
+            <li className="nav-item">
+              <a className="nav-link text-primary" href="#">
+                <img
+                  src="https://cdn2.iconfinder.com/data/icons/4web-3/139/favourite-512.png"
+                  alt="Heart"
+                  style={{ width: "20px", marginRight: "5px" }}
+                />
+                Favorites
+              </a>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle text-primary"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Art Categories
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Paintings
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Photography
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Digital Art
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="d-flex justify-content-center align-items-center my-3">
+          {" "}
+          <form className="d-flex" role="search">
+            <input className="form-control me-2 center-placeholder" type="search" placeholder="" aria-label="Search" />
+            <button className="red-search-button" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+        <ul className="navbar-nav ml-auto justify-content-end">
+          <li className="nav-item">
+            <a className="nav-link text-primary" href="#">
+              <img
+                src="https://previews.123rf.com/images/vectorlightstudio/vectorlightstudio2001/vectorlightstudio200100102/144161554-shopping-cart-icon.jpg"
+                alt="Cart"
+                style={{ width: "23px", marginRight: "5px" }}
+              />
+              Cart
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle text-primary"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Profile
+            </a>
+            <ul className="dropdown-menu dropdown-menu-end">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Register
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Sign In
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Log Out
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
